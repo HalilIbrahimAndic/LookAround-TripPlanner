25bb132c3b5d400c9453f752ee69d894c8336f6a
Halil İbrahim Andiç <hiandic@gmail.com>
2024-05-26 01:13:05 +0300

    Initial Commit

---
+++ LookAround/LookAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookAroundApp.swift
+//  LookAround
+//
+//  Created by handic on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LookAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
