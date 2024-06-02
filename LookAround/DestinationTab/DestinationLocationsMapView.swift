47aa63b44cb3a96599c2eb684553b0e05ac196bc
Halil İbrahim Andiç <hiandic@gmail.com>
2024-06-02 23:59:18 +0300

    Develop: Initial coordinate given

--- LookAround/DestinationTab/DestinationLocationsMapView.swift
+++ LookAround/DestinationTab/DestinationLocationsMapView.swift
@@ -6,10 +6,22 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct DestinationLocationsMapView: View {
+    
+    @State private var cameraPosition: MapCameraPosition = .automatic
+    
     var body: some View {
-        Text("Destination Locations Map")
+        Map(position: $cameraPosition)
+            .onAppear {
+                let coordinate = CLLocationCoordinate2D(latitude: 48.856788,
+                                                   longitude: 2.351077)
+                let span = MKCoordinateSpan(latitudeDelta: 0.15,
+                                            longitudeDelta: 0.15)
+                let region = MKCoordinateRegion(center: coordinate, span: span)
+                cameraPosition = .region(region)
+        }
     }
 }
 
