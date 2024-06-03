9f15cce38a7ba9e5263e5cfe71a4ff9a4d3b601c
Halil İbrahim Andiç <hiandic@gmail.com>
2024-06-03 07:58:15 +0300

    Refactor: Mock data replaced

--- LookAround/DestinationTab/DestinationLocationsMapView.swift
+++ LookAround/DestinationTab/DestinationLocationsMapView.swift
@@ -7,74 +7,32 @@
 
 import SwiftUI
 import MapKit
+import SwiftData
 
 struct DestinationLocationsMapView: View {
     
     @State private var cameraPosition: MapCameraPosition = .automatic
     @State private var visibleRegion: MKCoordinateRegion?
+    @Query private var destinations: [Destination]
+    @State private var destination: Destination?
     
     var body: some View {
         Map(position: $cameraPosition) {
-            Marker("Moulin Rouge", coordinate: .moulinRouge)
             
-            Marker(coordinate: .arcDeTriomphe) {
-                Label("Arc de Triomphe", systemImage: "star.fill")
-            }
-            .tint(.yellow)
-            
-            Marker("Gare du Nord", monogram: Text("GN"), coordinate: .gareDuNord)
-                .tint(.orange)
-            
-            Marker("Louvre", systemImage: "person.crop.artframe", coordinate: .louvre)
-                .tint(.main)
-            
-            Annotation("Notre Dame", coordinate: .notreDame) {
-                Image(systemName: "star")
-                    .imageScale(.large)
-                    .foregroundStyle(.red)
-                    .padding(10)
-                    .background(.white)
-                    .clipShape(.circle)
-            }
-            
-            Annotation("Sacre Coeur", coordinate: .sacreCoeur, anchor: .center) {
-                Image(.sacre)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 30, height: 30)
-            }
-            
-            Annotation("Pantheon", coordinate: .pantheon) {
-                Image(systemName: "mappin")
-                    .imageScale(.large)
-                    .foregroundStyle(.red)
-                    .padding(5)
-                    .overlay {
-                        Circle()
-                            .strokeBorder(.red, lineWidth: 2)
-                    }
-            }
-            
-            MapCircle(center: CLLocationCoordinate2D(
-                latitude: 48.856788,
-                longitude: 2.351077),
-                      radius: 5000)
-            .foregroundStyle(.red.opacity(0.5))
         }
         .onMapCameraChange(frequency: .onEnd) { context in
             visibleRegion = context.region
         }
-            .onAppear {
-                let coordinate = CLLocationCoordinate2D(latitude: 48.856788,
-                                                   longitude: 2.351077)
-                let span = MKCoordinateSpan(latitudeDelta: 0.15,
-                                            longitudeDelta: 0.15)
-                let region = MKCoordinateRegion(center: coordinate, span: span)
+        .onAppear {
+            destination = destinations.first
+            if let region = destination?.region {
                 cameraPosition = .region(region)
+            }
         }
     }
 }
 
 #Preview {
     DestinationLocationsMapView()
+        .modelContainer(Destination.preview)
 }
