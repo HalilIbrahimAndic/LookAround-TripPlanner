055b3b13343a1cabd4596dcd5afdfa8d1f20aa68
Halil İbrahim Andiç <hiandic@gmail.com>
2024-06-03 07:19:32 +0300

    Develop: MTPlacemark added

---
+++ LookAround/Models/MTPlacemark.swift
@@ -0,0 +1,24 @@
+//
+//  MTPlacemark.swift
+//  LookAround
+//
+//  Created by handic on 3.06.2024.
+//
+
+import SwiftData
+import MapKit
+
+@Model
+class MTPlacemark {
+    var name: String
+    var address: String
+    var latitude: Double
+    var longitude: Double
+    
+    init(name: String, address: String, latitude: Double, longitude: Double) {
+        self.name = name
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
